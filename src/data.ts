0aca0a24f6c63d6b77e5f366810a843233ea619b
albert.yim <albert.yim719@gmail.com>
2023-08-19 00:31:36 +0900

    Add data for enlist and discharge depend on ROKAF generation

---
+++ src/data.ts
@@ -0,0 +1,79 @@
+export const GENERATION: {
+  [generation: number]: {
+    enlist: string;
+    discharge: string;
+  };
+} = {
+  833: {
+    enlist: "2021-12-06",
+    discharge: "2023-09-05",
+  },
+  834: {
+    enlist: "2022-01-10",
+    discharge: "2023-10-09",
+  },
+  835: {
+    enlist: "2022-02-14",
+    discharge: "2023-11-13",
+  },
+  836: {
+    enlist: "2022-03-21",
+    discharge: "2023-12-20",
+  },
+  837: {
+    enlist: "2022-04-25",
+    discharge: "2024-01-24",
+  },
+  838: {
+    enlist: "2022-05-30",
+    discharge: "2024-02-29",
+  },
+  839: {
+    enlist: "2022-07-11",
+    discharge: "2024-04-10",
+  },
+  840: {
+    enlist: "2022-08-22",
+    discharge: "2024-05-21",
+  },
+  841: {
+    enlist: "2022-09-26",
+    discharge: "2024-06-25",
+  },
+  842: {
+    enlist: "2022-10-31",
+    discharge: "2024-07-30",
+  },
+  843: {
+    enlist: "2022-12-05",
+    discharge: "2024-09-04",
+  },
+  844: {
+    enlist: "2023-01-09",
+    discharge: "2024-10-08",
+  },
+  845: {
+    enlist: "2023-02-13",
+    discharge: "2024-11-12",
+  },
+  846: {
+    enlist: "2023-03-20",
+    discharge: "2024-12-19",
+  },
+  847: {
+    enlist: "2023-04-24",
+    discharge: "2025-01-23",
+  },
+  848: {
+    enlist: "2023-05-30",
+    discharge: "2025-02-28",
+  },
+  849: {
+    enlist: "2023-07-10",
+    discharge: "2025-04-09",
+  },
+  850: {
+    enlist: "2023-08-14",
+    discharge: " 2025-05-13",
+  },
+};
