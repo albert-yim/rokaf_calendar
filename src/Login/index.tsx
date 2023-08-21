f99bcf7f6d3023650dace38e27fb9191abeebaef
albert.yim <albert.yim719@gmail.com>
2023-08-21 22:46:43 +0900

    Implement the temp login logic

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,20 +1,56 @@
 import React, { useState } from "react";
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Select } from "antd";
 import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.scss";
 import { ValidateErrorEntity } from "rc-field-form/lib/interface";
+import { GENERATION } from "../data";
 // import { Input } from "../components";
 type LoginFormType = {
   username: string;
   password: string;
+  generation: number;
 };
+const USER_DATA = [
+  {
+    username: "test",
+    password: "asdqwe123!",
+    generation: 846,
+  },
+];
 export default function Login() {
+  const generation_key = Object.keys(GENERATION);
+  const [firstLogin, setFirstLogin] = useState(false);
   const [form] = Form.useForm<LoginFormType>();
   const navigate = useNavigate();
+  const login = (generation?: number) => {
+    if (generation) {
+      // set generation
+      console.log(`set generation ${generation}`);
+    }
+    navigate("/home", { replace: true });
+  };
   const onSubmit = (values: LoginFormType) => {
+    if (firstLogin) {
+      login(form.getFieldValue("generation"));
+      return;
+    }
+    const user = USER_DATA.find(
+      (user) => user.username === form.getFieldValue("username"),
+    );
+    if (!user) {
+      // create user
+      setFirstLogin(true);
+      return;
+    }
+    if (user?.password !== form.getFieldValue("password")) {
+      console.log("password error");
+      return;
+    }
     console.log(values);
     console.log("submit!");
+    login();
   };
+
   const onFailed = (error: ValidateErrorEntity<LoginFormType>) => {
     console.log(error);
     console.log("Failed");
@@ -37,7 +73,7 @@ export default function Login() {
           },
         ]}
       >
-        <Input />
+        <Input disabled={firstLogin} />
       </Form.Item>
       <Form.Item
         label="Password"
@@ -49,7 +85,16 @@ export default function Login() {
           },
         ]}
       >
-        <Input.Password />
+        <Input.Password disabled={firstLogin} />
+      </Form.Item>
+      <Form.Item label="기수" name="generation" hidden={!firstLogin}>
+        <Select placeholder="기수를 선택하세요." style={{ width: "100%" }}>
+          {generation_key.map((generation) => (
+            <Select.Option key={generation} value={generation}>
+              {generation}
+            </Select.Option>
+          ))}
+        </Select>
       </Form.Item>
       <Form.Item>
         <Button type="primary" htmlType="submit">
